8681eb9beec71aaedb6b52081968f337eb674123
Mayerstrk <45215789+Mayerstrk@users.noreply.github.com>
2023-10-15 21:46:48 +0300

    feature: add self signed openssl cerificate for https hosting and add to vite config, refactor(app.ts): changed cors origin from http to https

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -30,7 +30,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5173', 'http://localhost:4173'],
+		origin: ['https://localhost:5173', 'https://localhost:4173'],
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true,
 		optionsSuccessStatus: 204,
