64da667a122d5232b436c0d7934d5b994e5ac9c4
Mayerstrk <45215789+Mayerstrk@users.noreply.github.com>
2023-10-27 05:11:41 +0300

    refactor: remove certificates config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,7 @@
-import fs from 'node:fs';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		https: {
-			key: fs.readFileSync('./certs/privatekey.pem'),
-			cert: fs.readFileSync('./certs/cert.pem'),
-			passphrase: 'admin',
-		},
-	},
 });
