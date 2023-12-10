c6b8ec262debbe2148bb086f435e66f003aacfae
Mayer Starkman <mayerstrk@gmail.com>
2023-12-10 20:19:27 +0200

    fix(utils.ts): change paths to base /around/

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,7 +1,7 @@
 enum RoutesPaths {
-	home = '/',
-	signIn = '/signin',
-	signUp = '/signup',
+	home = '/around',
+	signIn = '/around/signin',
+	signUp = '/around/signup',
 }
 function isSuccessCode(status: number) {
 	if (status >= 200 && status < 300) {
